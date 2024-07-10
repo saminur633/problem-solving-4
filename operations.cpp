b4713f069eab2d31bdae1cb51a6b66204e30e490
saminur633 <rahmansaminur51@gmail.com>
2024-07-10 13:15:36 +0600

    added number operations

--- operations.cpp
+++ operations.cpp
@@ -21,3 +21,28 @@ using namespace std;
     return 0;
 }
 
+
+#include <iostream>
+using namespace std;
+
+
+
+  int main (){
+      int operations = 0;
+      for (int i =1 ; i<= 5; i++){
+            if (i == 4) continue;
+            for ( int j= 1; j<= 6; j++){
+                operations++;
+            }
+
+}
+ cout << operations << endl ;
+
+
+
+
+    return 0;
+}
+
+
+
