7a78e031da8e10cf5397b568bca5dc0ee926b3f9
saminur633 <rahmansaminur51@gmail.com>
2024-07-10 13:03:44 +0600

    added nested

--- index.cpp
+++ index.cpp
@@ -27,3 +27,18 @@ int main() {
     }
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int number = 1; number <= 10; number++) {
+        // we need to print the multiplication table for this number
+        // let's use a loop to do it and separate the numbers by a space
+        cout << number << ": ";
+        for (int i = 1; i <= 10; i++) {
+            cout << number * i << ' ';
+        }
+        cout << '\n'; // print each table in a new line
+    }
+    return 0;
+}
