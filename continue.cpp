0880e7f17fc602e4c71edb8fad97ff2c4d890cac
saminur633 <rahmansaminur51@gmail.com>
2024-07-10 12:59:27 +0600

    added continue statement

---
+++ continue.cpp
@@ -0,0 +1,18 @@
+// continue statement
+#include <iostream>
+using namespace std;
+
+
+
+  int main (){
+      for (int i =1 ; i<= 10; i++){
+            if ( i == 7) continue;
+    cout << i << endl ;
+}
+
+
+
+
+    return 0;
+}
+
