9c2218e3fa0f39b5ae861279ae12f702314a6f26
saminur633 <rahmansaminur51@gmail.com>
2024-07-10 12:54:00 +0600

    added break statement

--- break.cpp
+++ break.cpp
@@ -18,3 +18,24 @@ int main() {
     }
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    for (int week = 1; week <= 4; week++) {
+    cout << "Week = " << week << '\n';
+    for (int day = 1; day <= 7; day++) {
+        cout << "   Day = " << day << '\n';
+        if (day == 3) {
+            break; // This will exit the inner loop when 'day' is 3
+        }
+    }
+}
+
+
+
+    return 0;
+}
+
