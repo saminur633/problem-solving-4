6273ae0d2e16b9cf11e675edd964dd5ae15ca79a
saminur633 <rahmansaminur51@gmail.com>
2024-07-10 12:44:24 +0600

    added break statement

---
+++ break.cpp
@@ -0,0 +1,20 @@
+// break statement
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int week = 1; week <= 4; week++) {
+        cout << "week = " << week << '\n';
+        if (week == 3) break;
+       // for(int day = 1; day <= 7 ; day++){
+            //cout << " day = " << day << '\n';
+            int day = 1;
+            while (day <=7){
+            cout << " day = " << day << '\n';
+            day++;
+
+            }
+        //}
+    }
+    return 0;
+}
