26d38434bb9e70ae2afd1a56acc4b4d906eab7f7
saminur633 <rahmansaminur51@gmail.com>
2024-07-10 11:47:47 +0600

    added nested loop

---
+++ index.cpp
@@ -0,0 +1,10 @@
+// Nested loop
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int week = 1; week <= 4; week++) {
+        cout << "week = " << week << '\n';
+    }
+    return 0;
+}
